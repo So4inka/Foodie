87b8c77049afb8bd3ae424ac0f9bb65f871f1b02
Nadezda Kallaur <nadezdakallaur@Nadezdas-Air.fios-router.home>
2021-08-02 21:37:00 -0400

    few changes

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// test
+// Pseudo Code Steps for the Project:
+//Step 1: Fetch Country data using .then() 
+//from the API https://themealdb.com/
+// const DOMAIN = 'http://www.omdbapi.com/';
+// const API_KEY = 'd686a256'
+// const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&s=`;
+// axios.get("http://www.omdbapi.com/")
+//   .then((res) => {
+//     //handle success!
+//     // console.log(res.data)
+//     const data = res.data
+//     // DataCue.forEach((movie)) => {
+//     //   console.log(title)
+//     // })
+//     console.log(res.data)
+//   })
+  // .catch((error) => {
+  //  handle error
+  //   console.error(error)
+  // })
+  // .finally(() => {
+  //   //always executed
+// Step 2. // Make a request and convert response to dropdown menu 
+// for SEARCH BY AREA "http://www.themealdb.com/api/json/v1/1/list.php?a=list"
+// for search random "www.themealdb.com/api/json/v1/1/random.php"
+//
+//// Step 3: Create dynamic HTML
+// Step 4: Append Country Data
+// Step 5: Dynamically search country using HTML form with eventHandler
+// Step 6: Remove previous country data
+ // Make sure to call this function
+ // Step 6: Remove previous country data
+
+// function removeCountry() {
+//  // write code here
+//  // Make sure to call this function?
+
