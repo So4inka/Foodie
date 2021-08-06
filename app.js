d84c697e02f062397e76442a87df1ce2198dc331
Nadezda Kallaur <nadezdakallaur@Nadezdas-Air.fios-router.home>
2021-08-05 23:11:55 -0400

    all done but styling image not responsive !

--- app.js
+++ app.js
@@ -8,17 +8,14 @@ const fetchData = async (country) => {
   removeRecipe()
   try {
     const url = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
-    // const mealId = url.data.meals[0].idMeal
     console.log(url.data.meals)
     const mealsArr = url.data.meals
     for (let i = 0; i < 3; i++) {
       const meal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealsArr[i].idMeal}`)
       const mealRecipe = meal.data.meals[0]
       console.log(mealRecipe)
-      showMealRecipe(mealRecipe)
-      
-    }
-      
+      showMealRecipe(mealRecipe)  
+    }    
   }
   catch (error) {
     console.error(error)
@@ -65,42 +62,4 @@ function removeRecipe() {
 // Do Steps from 1-6 for SEARCH BY RANDOM "www.themealdb.com/api/json/v1/1/random.php";
 
 
-  // write code here
-    // removeRandom()
-    
   
-// strMeal
-// strMealThumb
-// strCategory
-// strArea
-// strInstructions:
-// strYoutube:
-// strIngredient:
-// strMeasure: 
-// Step 2. Create dynamic HTML for SEARCH RANDOM 
- 
-  // console.log(randomElements)
-// Step 4. Dynamicly search county using HTML form with eventHadler
-
-// Step 5. Remove previous random data
-// write  code here
-// call that function
-
-
-
-
-
-
-
-
-// for SEARCH BY RECIPE NAME
-//// Step 3: Create dynamic HTML
-// create img for each search result...
-// Step 4: Append  Recipe Data
-// Step 5: Dynamically search recipes using HTML form with eventHandler
-// Step 6: Remove previous search data
- // Make sure to call this function
- 
-// Search by county cuisine - total 32 counties & each country meals...
-// Do the same steps for search random recipes
-// Do the same steps for search by name of the 
